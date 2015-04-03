353c476d8186fa5d7d4d45f55a5ccbcbdff24aad
MarcoFinocchi <mrcfinocchi@gmail.com>
2015-04-03 19:01:37 +0200

    Sistemate alcune cosette delle classi ViewController.m e .h

--- Rate It!/ViewController.h
+++ Rate It!/ViewController.h
@@ -5,9 +5,10 @@
 
 @property (nonatomic,strong) Poll *p;
 @property (nonatomic,weak) IBOutlet UILabel *name;
-@property (weak, nonatomic) IBOutlet UITextView *description;
-@property (weak, nonatomic) IBOutlet UILabel *deadline;
-@property (weak, nonatomic) IBOutlet UILabel *lastUpdate;
-@property (weak, nonatomic) IBOutlet UIButton *vota;
+@property (nonatomic,weak) IBOutlet UITextView *description;
+@property (nonatomic,weak) IBOutlet UIImageView *image;
+@property (nonatomic,weak) IBOutlet UILabel *deadline;
+@property (nonatomic,weak) IBOutlet UILabel *lastUpdate;
+@property (nonatomic,weak) IBOutlet UIButton *vota;
 
 @end
--- Rate It!/ViewController.m
+++ Rate It!/ViewController.m
@@ -8,7 +8,7 @@
 
 @implementation ViewController
 
-@synthesize p,name,deadline,lastUpdate,description,vota;
+@synthesize p,name,description,image,deadline,lastUpdate,vota;
 
 - (void)viewDidLoad {
     
@@ -18,15 +18,15 @@
     name.text = p.pollName;
     NSString *strDeadline = @"Scadenza: ";
     strDeadline = [strDeadline stringByAppendingString:(NSString *)p.deadline];
-    deadline.font = [UIFont fontWithName:@"ChalkboardSE-Light" size:14];
+    deadline.font = [UIFont fontWithName:@"ChalkboardSE-Light" size:15];
     deadline.textColor = [UIColor redColor];
     deadline.text = strDeadline;
     NSString *strLastUpdate = @"Ultima modifica: ";
     strLastUpdate = [strLastUpdate stringByAppendingString:(NSString *)p.lastUpdate];
-    lastUpdate.font = [UIFont fontWithName:@"ChalkboardSE-Light" size:14];;
+    lastUpdate.font = [UIFont fontWithName:@"ChalkboardSE-Light" size:15];;
     lastUpdate.text = strLastUpdate;
     description.selectable = true;
-    description.font = [UIFont fontWithName:@"ChalkboardSE-Regular" size:18];
+    description.font = [UIFont fontWithName:@"ChalkboardSE-Regular" size:16];
     description.textAlignment = NSTextAlignmentNatural;
     description.text = p.pollDescription;
     description.selectable = false;
